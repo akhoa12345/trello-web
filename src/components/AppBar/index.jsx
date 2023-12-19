102ac1427ab809ba6d229faa6521b1234d3c0a68
Anh Khoa <phunganhkhoa123@gmail.com>
2023-12-19 22:26:52 +0700

    21. Code thanh Header: Trello App Bar (part 1)

--- src/components/AppBar/index.jsx
+++ src/components/AppBar/index.jsx
@@ -1,19 +1,78 @@
 import Box from '@mui/material/Box'
+import SvgIcon from '@mui/material/SvgIcon'
+import Typography from '@mui/material/Typography'
+import Button from '@mui/material/Button'
+import TextField from '@mui/material/TextField'
+import Badge from '@mui/material/Badge'
+import Tooltip from '@mui/material/Tooltip'
+import AppsIcon from '@mui/icons-material/Apps'
+import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone'
+import HelpOutlineIcon from '@mui/icons-material/HelpOutline'
 
 import ModeSelect from '~/components/ModeSelect'
+import TrelloIcon from '~/assets/trello.svg?react'
+import Workspaces from './Menu/Workspaces'
+import Recent from './Menu/Recent'
+import Starred from './Menu/Starred'
+import Templates from './Menu/Templates'
+import Profile from './Menu/Profile'
 
 function AppBar() {
   return (
     <Box
+      px={2}
       sx={{
-        backgroundColor: 'primary.light',
         width: '100%',
         height: (theme) => theme.trello.appBarHeight,
         display: 'flex',
-        alignItems: 'center'
+        alignItems: 'center',
+        justifyContent: 'space-between'
       }}
     >
-      <ModeSelect />
+      <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
+        <AppsIcon sx={{ color: 'primary.main' }} />
+        <Box sx={{ display: 'flex', alignItems: 'center', gap: 0.5 }}>
+          <SvgIcon
+            component={TrelloIcon}
+            inheritViewBox
+            sx={{ color: 'primary.main' }}
+          />
+          <Typography
+            variant='span'
+            sx={{
+              fontSize: '1.2rem',
+              fontWeight: 'bold',
+              color: 'primary.main'
+            }}
+          >
+            Trello
+          </Typography>
+        </Box>
+
+        <Workspaces />
+        <Recent />
+        <Starred />
+        <Templates />
+
+        <Button variant="outlined">Create</Button>
+      </Box>
+
+      <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
+        <TextField id="outlined-search" label="Search..." type="search" size='small' />
+        <ModeSelect />
+
+        <Tooltip title="Notification">
+          <Badge color="secondary" variant="dot" sx={{ cursor: 'pointer' }}>
+            <NotificationsNoneIcon />
+          </Badge>
+        </Tooltip>
+
+        <Tooltip title="Help">
+          <HelpOutlineIcon sx={{ cursor: 'pointer' }} />
+        </Tooltip>
+
+        <Profile />
+      </Box>
     </Box>
   )
 }
