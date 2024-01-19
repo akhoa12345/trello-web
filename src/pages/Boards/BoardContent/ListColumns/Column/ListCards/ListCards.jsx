ba6626bafb51346fba5a2a7d13d7e537e76ef0fc
Anh Khoa <phunganhkhoa123@gmail.com>
2024-01-19 09:53:02 +0700

    32. Kéo thả Trello Card với Dnd-kit: DragOverlay

--- src/pages/Boards/BoardContent/ListColumns/Column/ListCards/ListCards.jsx
+++ src/pages/Boards/BoardContent/ListColumns/Column/ListCards/ListCards.jsx
@@ -1,37 +1,40 @@
 import Box from '@mui/material/Box'
+import { SortableContext, verticalListSortingStrategy } from '@dnd-kit/sortable'
 
 import Card from './Card/Card'
 
 function ListCards({ cards }) {
   return (
-    <Box sx={{
-      p: '0 5px',
-      m: '0 5px',
-      display: 'flex',
-      flexDirection: 'column',
-      gap: 1,
-      overflowX: 'hidden',
-      overflowY: 'auto',
-      maxHeight: (theme) => `calc(
-        ${theme.trello.boardContentHeight} - 
-        ${theme.spacing(5)} -
-        ${theme.trello.columnHeaderHeight} -
-        ${theme.trello.columnFooterHeight}
-      )`,
-      '&::-webkit-scrollbar-thumb': {
-        backgroundColor: '#ced0da'
-      },
-      '&::-webkit-scrollbar-thumb:hover': {
-        backgroundColor: '#bfc2cf'
-      }
-    }}>
+    <SortableContext items={cards?.map(card => card._id)} strategy={verticalListSortingStrategy}>
+      <Box sx={{
+        p: '0 5px',
+        m: '0 5px',
+        display: 'flex',
+        flexDirection: 'column',
+        gap: 1,
+        overflowX: 'hidden',
+        overflowY: 'auto',
+        maxHeight: (theme) => `calc(
+          ${theme.trello.boardContentHeight} - 
+          ${theme.spacing(5)} -
+          ${theme.trello.columnHeaderHeight} -
+          ${theme.trello.columnFooterHeight}
+        )`,
+        '&::-webkit-scrollbar-thumb': {
+          backgroundColor: '#ced0da'
+        },
+        '&::-webkit-scrollbar-thumb:hover': {
+          backgroundColor: '#bfc2cf'
+        }
+      }}>
 
-      {cards?.map((card) => {
-        return (
-          <Card key={card._id} card={card} />
-        )
-      })}
-    </Box>
+        {cards?.map((card) => {
+          return (
+            <Card key={card._id} card={card} />
+          )
+        })}
+      </Box>
+    </SortableContext>
   )
 }
 
