29f1e5b69a8b05de3cb39937bf7b1508a3ef58b4
Anh Khoa <phunganhkhoa123@gmail.com>
2024-02-03 10:06:28 +0700

    61. Axios: Gọi API từ phía Front-end sao cho Clean Code

--- src/pages/Boards/_id.jsx
+++ src/pages/Boards/_id.jsx
@@ -1,16 +1,30 @@
+import { useEffect, useState } from 'react'
 import Container from '@mui/material/Container'
 
 import AppBar from '~/components/AppBar/AppBar'
 import BoardBar from '~/pages/Boards/BoardBar/BoardBar'
 import BoardContent from '~/pages/Boards/BoardContent/BoardContent'
-import { mockData } from '~/apis/mock-data'
+import { fetchBoardDetailAPI } from '~/apis'
 
 function Board() {
+  const [board, setBoard] = useState(null)
+
+  useEffect(() => {
+    //  Tạm thời fix cứng boardId
+    const boardId = '65bd117cf7a7f5ac8904e504'
+    // Call API
+    fetchBoardDetailAPI(boardId)
+      .then((board) => {
+        setBoard(board)
+      })
+    // console.log('board: ', board)
+  }, [])
+
   return (
     <Container disableGutters maxWidth={false} sx={{ height: '100vh' }}>
       <AppBar />
-      <BoardBar board={mockData?.board} />
-      <BoardContent board={mockData?.board} />
+      <BoardBar board={board} />
+      <BoardContent board={board} />
     </Container>
   )
 }
